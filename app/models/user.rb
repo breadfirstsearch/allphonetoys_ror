5294adf633c9539c36a50908fd2f35a9405d24ca
Jibin Rajan Varghese <email.jrv@gmail.com>
2017-11-16 12:09:26 -0600

    Fixing CAST issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,14 +19,13 @@ class User < ApplicationRecord
   def self.search(search_name, search_phone , search_amount, search_email, search_provider, search_location )
 
     @users = User.all
-    search_name ? @users = User.where('name LIKE ?', "%#{search_name}%") : @users
-    search_amount ? @users = @users.where('pref_amount LIKE ?', "%#{search_amount}%")  : @users
-    search_phone ? @users = @users.where('phone LIKE ?', "%#{search_phone}%")  : @users
-    search_email ? @users = @users.where('email LIKE ?', "%#{search_email}%")  : @users
-    search_provider ? @users = @users.where('pref_provider LIKE ?', "%#{search_provider}%")  : @users
-    search_location ? @users = @users.where('pref_location LIKE ?', "%#{search_location}%")  : @users
+    search_name ? @users = User.where('CAST(name as TEXT) LIKE ?', "%#{search_name}%") : @users
+    search_amount ? @users = @users.where('CAST(pref_amount as TEXT) LIKE ?', "%#{search_amount}%")  : @users
+    search_phone ? @users = @users.where('CAST(phone as TEXT)  LIKE ?', "%#{search_phone}%")  : @users
+    search_email ? @users = @users.where('CAST(email as TEXT)  LIKE ?', "%#{search_email}%")  : @users
+    search_provider ? @users = @users.where('CAST(pref_provider as TEXT)  LIKE ?', "%#{search_provider}%")  : @users
+    search_location ? @users = @users.where('CAST(pref_location as TEXT) LIKE ?', "%#{search_location}%")  : @users
 
   end
-
 
 end
