7051463b8112b6c00aec7c3e2fda22125512c5c9
habib_baig.tamu <habib_baig.tamu@tamu.edu>
2017-11-19 00:54:54 -0500

    password reset heruko

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' } 
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+
+config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain: "myapp.herokuapp.com",
+    :authentication => "plain",
+    :enable_starttls_auto => true,
+    :user_name => "habib1.baig@gmail.com",
+    :password => "Alpacino1!"
+  }
