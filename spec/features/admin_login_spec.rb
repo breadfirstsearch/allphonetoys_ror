570db979d6fd2e14e23602fa2b146049bb5653cb
Venkata Satya Kavya Sree Bondapalli <kavyasree.bvs@tamu.edu>
2017-11-27 03:22:08 +0000

    admin login rspec done

---
+++ spec/features/admin_login_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+describe "On the homepage, admins" do
+    
+    it 'can sign up successfully' do
+        #Create admin first
+        visit '/admins'
+        expect(page).to have_content("New Admin")
+        click_link "New Admin"
+        expect(page).to have_content("Email")
+        
+        fill_in('admin_name', :with => 'shaik1')
+        fill_in('admin_email', :with => 'shaik1@gmail.com')
+        fill_in('admin_password', :with => 'shaik123')
+        click_button('Create Admin')
+        
+        #debugger
+        expect(page).to have_content("Admin was successfully created.")
+    end
+    
+   before(:each) do
+        #Create admin first
+        visit '/admins'
+        expect(page).to have_content("New Admin")
+        click_link "New Admin"
+        expect(page).to have_content("Email")
+        
+        fill_in('admin_name', :with => 'shaik')
+        fill_in('admin_email', :with => 'shaik@gmail.com')
+        fill_in('admin_password', :with => 'shaik123')
+        click_button('Create Admin')
+        
+        #debugger
+        expect(page).to have_content("Admin was successfully created.")
+    end
+    it 'can sign in with correct Email and Password' do
+        #Test Login 
+        visit '/login'
+        expect(page).to have_text("Don't have an account?")
+        fill_in 'email', :with => "shaik@gmail.com"
+        fill_in 'password', :with => "shaik123"
+        click_button 'Log in'
+        expect(page).to have_text("Admin Dashboard")
+    end
+end
