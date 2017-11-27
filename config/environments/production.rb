f8543f20a4c1e2248932dbeb5ccd91176c49eee7
habib_baig.tamu <habib_baig.tamu@tamu.edu>
2017-11-27 03:35:58 -0500

    "adding jrv-dev as domain"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' } 
+config.action_mailer.default_url_options = { :host => 'jrv-develop.herokuapp.com' } 
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
